d111fa47c332e76ef72a008a03ac4ffd171215cf
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-08-17 09:38:23 +0200

    logout and navbar changed

--- Login/logout.php
+++ Login/logout.php
@@ -1,13 +1,20 @@
 
 <?php
 session_start();
+if (!isset($_SESSION['admin']) && !isset($_SESSION['user'])) {
+  header("Location: ../home_user/index.php");
+  exit;
+} else if (isset($_SESSION['user']) != "") {
+  header("Location: ../Login/login.php");
+} else if (isset($_SESSION['admin']) != "") {
+  header("Location: ../Login/login.php");
+}
 
-
-if ( isset($_GET['logout'])) { 
-  unset($_SESSION['user' ]); 
-  unset ($_SESSION['adm']);
-  session_unset(); 
+if (isset($_GET['logout'])) {
+  unset($_SESSION['user']);
+  unset($_SESSION['admin']);
+  session_unset();
   session_destroy();
-
-  header("Location:login.php" );
+  header("Location: ../home_user/index.php");
+  exit;
 }
--- components/navbar.php
+++ components/navbar.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,6 +22,7 @@
         opacity: 0;
         transform: translateY(-20px);
       }
+
       to {
         opacity: 1;
         transform: translateY(0);
@@ -56,39 +58,44 @@
     }
   </style>
 </head>
+
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-dark">
-  <div class="container">
-    <a class="navbar-brand" href="#">Meal Planner</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Recipes</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Meal Planner</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="../home_user/create_rec.php?id">Create Recipes</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="../Login/login.php">Login</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="../Login/register.php">Sign Up</a>
-        </li>
-      </ul>
+  <nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">Meal Planner</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Recipes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Meal Planner</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../home_user/create_rec.php?id">Create Recipes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../Login/login.php">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../Login/logout.php">Logout</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../Login/register.php">Sign Up</a>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
