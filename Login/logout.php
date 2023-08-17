0762ece70c404927f19432fbccea8c8d53fb2922
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-08-17 09:59:18 +0200

    login

--- Login/logout.php
+++ Login/logout.php
@@ -15,6 +15,6 @@ if (isset($_GET['logout'])) {
   unset($_SESSION['admin']);
   session_unset();
   session_destroy();
-  header("Location: ../home_user/index.php");
+  header("Location: ../Login/login.php");
   exit;
 }
