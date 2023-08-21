f881dfbd4f682203b287f0f3284e41772af54d08
Emelin Bilajbegovic <bilajbeg@gmail.com>
2023-08-21 11:20:12 +0200

    update admin folder

--- admin/create_user.php
+++ admin/create_user.php
@@ -3,6 +3,7 @@ include "../db_connect.php";
 $error = false;
 $fname = $lname = $email = $password = $role = $picture = "";
 $errorfname = $errorlname = $erroremail = $errorpassword = $errorpicture = "";
+
 function clear($input)
 {
     $data = trim($input);
@@ -57,6 +58,57 @@ if (isset($_POST["sign-up"])) {
         $password = hash("sha256", $password);
         $sql = "INSERT INTO `users`( `fname`, `lname`, `email`, `pass`,  `picture`) VALUES ('$fname','$lname','$email','$password','$picture')";
         $result = mysqli_query($connect, $sql);
-        header("Location: login.php");
+        header("Location: /Login/login.php");
     }
 }
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <h1 class="text-center">Sign up new user</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="fname" class="form-label">First name</label>
+                <input type="text" class="form-control" id="fname" name="fname" placeholder="" value="<?= $fname ?>">
+                <span class="text-danger"><?= $errorfname ?></span>
+                <div class="mb-3">
+                    <label for="lname" class="form-label">Last name</label>
+                    <input type="text" class="form-control" id="lname" name="lname" placeholder="" value="<?= $lname ?>">
+                    <span class="text-danger"><?= $errorlname ?></span>
+
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email address</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="" value="<?= $email ?>">
+                        <span class="text-danger"><?= $erroremail ?></span>
+                    </div>
+                    <label for="picture" class="form-label">Image link</label>
+                    <input type="text" class="form-control" id="picture" name="picture" placeholder="" value="<?= $picture ?>">
+                    <span class="text-danger"><?= $errorpicture ?></span>
+                    <div class="mb-3">
+
+
+                        <div class="mb-3">
+                            <label for="pass" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="pass" name="pass">
+                            <span class="text-danger"><?= $errorpassword ?></span>
+                        </div>
+                        <button name="sign-up" type="submit" class="btn btn-primary">Create</button>
+
+                        <span>You have an account? <a href="/Login/login.php">Login here</a></span>
+        </form>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+
+</html>
--- admin/update_user.php
+++ admin/update_user.php
@@ -2,46 +2,64 @@
 include "../db_connect.php";
 
 session_start();
-if(!isset($_SESSION["user"]) && !isset($_SESSION["admin"])){ 
-    header("Location:../Login/login.php"); }
-
-if(isset($_SESSION["user"])){ 
-    header("Location:../home_user/home.php"); 
+if (!isset($_SESSION["user"]) && !isset($_SESSION["admin"])) {
+    header("Location:../Login/login.php");
 }
 
-$sql="SELECT * FROM `users`";
-$result=mysqli_query($connect,$sql);
-$row=mysqli_fetch_assoc($result);
+if (isset($_SESSION["user"])) {
+    header("Location:../home_user/home.php");
+}
+
+$id = $_GET["id"];
+$sql = "SELECT * FROM users WHERE user_id = $id ";
+$result = mysqli_query($connect, $sql);
+$row = mysqli_fetch_assoc($result);
+
+if (isset($_POST["update"])) {
+    $fname = $_POST["fname"];
+    $lname = $_POST["lname"];
+    $email = $_POST["email"];
+    $picture = $_POST["picture"];
 
 
-if(isset($_POST["update"])){
-$fname=clear($_POST["fname"]);
-$lname=clear($_POST["lname"]);
-$email=clear($_POST["email"]);
-$picture=$_POST["picture"];
-   
-        $sql="UPDATE `users` SET `fname`='$fname',`lname`='$lname',`email`='$email',`picture`='$picture'";
-       $result=mysqli_query($connect,$sql);
+    /* function clear($input)
+{
+    $data = trim($input);
+    $data = strip_tags($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
 
-    if($result){
-        echo "success updateing";
-    }else{
+if (isset($_POST["update"])) {
+    $fname = clear($_POST["fname"]);
+    $lname = clear($_POST["lname"]);
+    $email = clear($_POST["email"]);
+    $picture = $_POST["picture"];
+ */
+    $sql = "UPDATE `users` SET `fname`='$fname',`lname`='$lname',`email`='$email',`picture`='$picture' WHERE user_id=$id ";
+    $result = mysqli_query($connect, $sql);
+
+    if ($result) {
+        echo "success updating";
+        header("refresh: 3; url = dashboard_users.php");
+    } else {
         echo "error";
     }
-    
-    }
+}
 ?>
 
 
 <!doctype html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Update page</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    </head>
-    <body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Update page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+
+<body>
     <div class="container mt-5">
         <h2>Update users </h2>
         <form method="POST" enctype="multipart/form-data">
@@ -53,21 +71,22 @@ $picture=$_POST["picture"];
                 <label for="lname" class="form-label">Last name</label>
                 <input type="text" class="form-control" value="<?= $row["lname"] ?>" id="lname" aria-describedby="lname" name="lname">
             </div>
-            
+
             <div class="mb-3">
                 <label for="email" class="form-label">Email</label>
-                <input type="text" class="form-control"value="<?= $row["email"] ?>" id="description" aria-describedby="email" name="email">
+                <input type="text" class="form-control" value="<?= $row["email"] ?>" id="description" aria-describedby="email" name="email">
             </div>
             <div class="mb-3">
                 <label for="picture" class="form-label">Picture link</label>
-                <input type="text" class="form-control"value="<?= $row["picture"] ?>" id="picture" aria-describedby="picture" name="picture">
+                <input type="text" class="form-control" value="<?= $row["picture"] ?>" id="picture" aria-describedby="picture" name="picture">
             </div>
-            
-            
-            <button name="update" type="submit" class="btn btn-primary">Update product</button>
-            <a href="../dashboard.php" class="btn btn-warning">Back to home page</a>
+
+
+            <button name="update" type="submit" class="btn btn-primary">Update user</button>
+            <a href="dashboard_users.php" class="btn btn-warning">Back to home page</a>
         </form>
     </div>
-    
+
 </body>
+
 </html>
