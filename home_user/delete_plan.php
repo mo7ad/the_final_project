a2c6ca8e2b392501f03c020d7958c78752e36909
borna.tikvica <bornatikvica@gmail.com>
2023-08-21 10:19:06 +0200

    dsds

--- home_user/delete_plan.php
+++ home_user/delete_plan.php
@@ -2,7 +2,7 @@
      require_once "../db_connect.php";
 
      $id = $_GET["id"];
-     $sql = "DELETE FROM `meal_planner` WHERE fk_recipes_id = $id";
+     $sql = "DELETE FROM `meal_planner` WHERE planner_id = $id";
      if(mysqli_query($connect, $sql)){
         header("location: meal_planner.php");
      }else{
